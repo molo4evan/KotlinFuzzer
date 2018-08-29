722cf0267fca39aaf271c4aaebbc50ab2892ce8e
molo4evan <Kuska1996327>
2018-08-29 17:01:07 +0700

    Added copying crashed files and crash reports for reusing

---
+++ found_bugs/001/Test_9.kt
@@ -0,0 +1,523 @@
+
+fun fun_0(arg_0: Int, arg_1: Byte, arg_2: Double): String {
+    var var_1 = 0
+    val var_2: Long = (6246028006676961280L).toLong()
+    while (var_1 < 4 && (false == !false || true) <= (false && false) && (false && 4981622164497410048L >= var_2 ushr 977277880) > false) {
+        var var_3: Int
+        var_1++
+        var var_4: Double = (arg_2).toDouble()
+        var_4++
+    }
+
+    run {
+        when ("gfx" + "yykujxnk") {
+            "wyrlybrjc" + "ysjyahfk" + ("xscemgbc" + "uopjmc") -> {
+                var var_5: Boolean
+            }
+            "vb" + "" -> {
+            }
+        }
+
+        when ("ucpc" + ("jfdent" + ("o" + ("qla").toString()) + "vfgytid") + ("aq" + ("guju" + "w"))) {
+            "jscesr" -> {
+            }
+        }
+
+        when (("sxiis").toString()) {
+            else -> {
+                var_1 /= arg_0
+            }
+        }
+
+        val var_6: Int = (1574799492).toInt()
+        var var_7: Char = ('d').toChar()
+        --var_7
+    }
+
+    if (true) {
+        var var_8: Long
+        var var_9: Boolean = false
+    }
+    else {
+        var var_10: Char = ('y').toChar()
+        return "de" + ("pvw" + "axb" + ("" + ("" + "ipua")))
+    }
+
+    run {
+        var_1 = (var_1 ushr (arg_0 xor arg_0)) - var_1
+    }
+
+    var var_11: String
+    run {
+        var var_12 = 0
+        var_11 = "falo"
+        while (var_12 < 1) {
+            val var_13: Boolean = false
+            var_12++
+            val var_14: Boolean = false
+            var var_15: String = ("big").toString()
+        }
+
+        var var_16: Char = ('E').toChar()
+        val var_17: Short = ((-22770).toShort()).toShort()
+        var var_18: Boolean = arg_1 > arg_1
+        var var_19: Double
+    }
+
+    var var_20 = 0
+    val var_21: Short = ((7059).toShort()).toShort()
+    while (var_20 < 5 && arg_1 < arg_1 && false) {
+        --var_20
+        var_20++
+        var var_22: Long
+        var_22 = var_2
+    }
+
+    run {
+        val var_23: Short = ((-20366).toShort()).toShort()
+    }
+
+    var_20 %= 1000209762
+    var var_24 = 0
+    var var_25: Long = (var_2).toLong()
+    while (var_24 < 1) {
+        var var_26: Short
+        var_24++
+        ++var_25
+        var var_27: Boolean = var_21 <= (-24517).toShort()
+    }
+
+    var var_28: Long
+    val var_29: Boolean = false
+    var var_30: Short
+    ++var_25
+    var_11 = "hjqybn" + "arcqkta"
+    return "oeprai" + "sfnmx"
+}
+
+
+fun fun_1(arg_0: Byte, arg_1: Double, arg_2: Any): Byte {
+    var var_31: Any
+    run {
+        val var_32: Byte = ((112).toByte()).toByte()
+        var var_33: Float = (2.89606E38F).toFloat()
+        --var_33
+        var var_34: Byte = ((-3).toByte()).toByte()
+        var var_35 = 0
+        val var_36: Boolean = true
+        while (var_35 < 3) {
+            var var_37: Float = (1.70697E38F).toFloat()
+            var_35++
+            var var_38: Char
+            var_34--
+        }
+
+        when (var_32) {
+            (var_34).toByte() -> {
+                var var_39: Long = (2170675695078914048L).toLong()
+            }
+            var_34 -> {
+            }
+        }
+
+        var var_40: Byte
+        var_31 = "ppo"
+    }
+
+    run {
+        val var_41: String = "etb"
+    }
+
+    var var_42: Float = (5.453134E37F).toFloat()
+    val var_43: Byte = ((113).toByte()).toByte()
+    when ((-88).toByte()) {
+        var_43 -> {
+        }
+        (80).toByte() -> {
+            var var_44: Short
+            return (var_43).toByte()
+        }
+        (-55).toByte() -> {
+        }
+        arg_0 -> {
+        }
+        else -> {
+            var var_45: Int
+            return arg_0
+        }
+    }
+
+    var var_46: Float
+    var var_47: Double = ((arg_1).toDouble()).toDouble()
+    run {
+        var var_48: String = "udxbjnsm"
+    }
+
+    when ((-60).toByte()) {
+        (65).toByte() -> {
+        }
+    }
+
+    run {
+        var_47--
+    }
+
+    var_31 = "n"
+    val var_49: Short = ((-1932).toShort()).toShort()
+    return var_43
+}
+
+
+fun fun_2(arg_0: Long, arg_1: Char, arg_2: Short, arg_3: Double): Int {
+    var var_50: Short
+    var var_51 = 0
+    var var_52: Short
+    while (var_51 < 2 && 1.1902547E37F <= (2.268729E38F).toFloat() && !false) {
+        var var_53: Int
+        var_51++
+        var var_54: String = "pbnx" + ("hnjh" + ("kmvvtokk").toString()) + "tsdrc" + ((("bvdcvduy").toString() + "j")).toString()
+        val var_55: Long = (7969033450833640448L).toLong()
+    }
+
+    run {
+        var var_56: Short
+    }
+
+    --var_51
+    var var_57: Short
+    return var_51
+}
+
+
+fun fun_3(arg_0: Long, arg_1: Double, arg_2: Byte): String {
+    run {
+        when ("xkn") {
+            "oxw" -> {
+                val var_58: Char = ('I').toChar()
+                var var_59: Float
+            }
+            "lkmksglw" -> {
+            }
+            "aue" -> {
+            }
+            "p" -> {
+            }
+        }
+
+        run {
+            val var_60: Double = (1.4156445E308).toDouble()
+            val var_61: Float = (2.6669738E38F).toFloat()
+            var var_62: Boolean = false
+        }
+
+        val var_63: String = "fq"
+        var var_64: Long = (arg_0).toLong()
+        var var_65: Byte = (arg_2).toByte()
+        var var_66: Long
+        val var_67: Boolean = true
+    }
+
+    var var_68: Long
+    var var_69: Double = (1.3815776E308).toDouble()
+    if (false || false) {
+        val var_70: String = "cea"
+        return "lmbej"
+    }
+    else {
+        var var_71: Long
+    }
+
+    if (2.5022119E38F < 2.2112857E38F != arg_0 <= 7772989228872066048L) {
+        val var_72: Int = (520875049).toInt()
+        val var_73: Double = (1.0595485E308).toDouble()
+        var var_74: Short
+        var var_75: Long
+        return "d" + "kxlar" + fun_0(1494950067, (-46).toByte(), 1.1940178E308)
+    }
+    else {
+        val var_76: Char = ('x').toChar()
+    }
+
+    if (var_69 < arg_1) {
+        var var_77: Char
+    }
+    else {
+        var_68 = 3841963887779708928L and arg_0
+        var var_78: Byte = (arg_2).toByte()
+        return fun_0(816306725 shr ((fun_2(arg_0, 'W', (26202).toShort(), 6.7278689E307)).toInt() xor -fun_2(8394673395462803456L, 'F', (28815).toShort(), var_69)), arg_2, 5.0119972E307)
+    }
+
+    when ("hnqmypsmp") {
+        "lt" -> {
+        }
+    }
+
+    when ("" + "yigj") {
+        "hfeod" -> {
+            var var_79: Float
+            return "" + "kqckb" + ""
+        }
+        "iusilkn" -> {
+        }
+        "wematu" -> {
+        }
+    }
+
+    var var_80: Double = (6.0075925E307).toDouble()
+    val var_81: Float = (1.5906389E38F).toFloat()
+    val var_82: Short = ((-30373).toShort()).toShort()
+    return "quh"
+}
+
+
+fun fun_4(arg_0: Boolean): Unit {
+    if (arg_0) {
+        val var_83: String = ("oebalmax").toString()
+        var var_84: Float
+    }
+    else {
+        var var_85: Char = ('d').toChar()
+    }
+
+    val var_86: Short = ((27451).toShort()).toShort()
+    var var_87: Double
+    return 
+}
+
+
+fun fun_5(arg_0: String, arg_1: Float, arg_2: Char): String {
+    when (arg_0) {
+        ("jkcrxuofa").toString() + "ixwc" -> {
+        }
+    }
+
+    fun_4(arg_1 < arg_1 > !true)
+    fun_4(-151912246 < 686548762)
+    fun_4(true || (1757279171 or -fun_2(1678715684402281472L, 'R', (-28665).toShort(), 4.5553967E306)) + - ((1047342790).toInt()) > + (1864888757 shl (65076501 or 1318799571)))
+    return "kiouvi"
+}
+
+
+fun fun_6(): Boolean {
+    fun_4(false || 1653732900386178048L >= (6789493535570983936L or 1069236132906818560L))
+    var var_88 = 0
+    var var_89: Char
+    while (var_88 < 1 && false) {
+        fun_4((108).toByte() <= (112).toByte())
+        var_88++
+        fun_4(true)
+        var var_90: Int
+    }
+
+    var_89 = 'h'
+    fun_4(false)
+    var_89 = 'i'
+    fun_4((39).toByte() < ((62).toByte()).toByte())
+    return ('j').toChar() >= 'c'
+}
+
+
+fun fun_7(arg_0: Short, arg_1: Short, arg_2: Int, arg_3: Boolean): Short {
+    run {
+        fun_4(arg_3)
+        if (! (7.9713983E307 < 1.6873047E308)) {
+            fun_4(arg_3)
+        }
+        else {
+            fun_4(arg_3)
+        }
+
+        run {
+            fun_4(arg_3)
+        }
+
+        when ((11563).toShort()) {
+            (5444).toShort() -> {
+            }
+            arg_0 -> {
+            }
+        }
+
+        fun_4(arg_3)
+        fun_4(2.6956243E38F >= 2.0341965E38F)
+    }
+
+    fun_4(false)
+    fun_4(arg_3)
+    when (arg_1) {
+        (-29229).toShort() -> {
+        }
+    }
+
+    if (!arg_3) {
+        fun_4(arg_3)
+    }
+    else {
+        when ((30787).toShort()) {
+            arg_0 -> {
+            }
+            else -> {
+                fun_4(true)
+                return (19427).toShort()
+            }
+        }
+
+        fun_4(true)
+        fun_4(fun_6())
+        return ((23164).toShort()).toShort()
+    }
+
+    fun_4(arg_3)
+    var var_91: Int
+    fun_4(true && arg_3 == arg_3 && arg_3)
+    fun_4(fun_6())
+    fun_4(arg_3)
+    fun_4(true)
+    fun_4(false)
+    val var_92: Boolean = false
+    return (22465).toShort()
+}
+
+
+fun fun_8(arg_0: Long, arg_1: Short, arg_2: Char): Byte {
+    var var_93: Boolean
+    fun_4(fun_6())
+    var var_94: Long
+    fun_4(arg_1 <= (-4476).toShort())
+    when (((-1).toByte()).toByte()) {
+        (-125).toByte() -> {
+        }
+        (-96).toByte() -> {
+        }
+    }
+
+    fun_4(false)
+    fun_4(false)
+    return (-105).toByte()
+}
+
+
+fun fun_9(arg_0: Short, arg_1: Int, arg_2: Double, arg_3: Byte): Double {
+    fun_4(!true)
+    var var_95: Double = (1.140032E308).toDouble()
+    when (7.4034217E305) {
+        1.6997905E308 -> {
+            var var_96: Long = (8888928414249218048L).toLong()
+            return arg_2
+        }
+        var_95 -> {
+        }
+        7.3778004E307 -> {
+        }
+        var_95 -> {
+        }
+        var_95 -> {
+        }
+        var_95 -> {
+        }
+        arg_2 -> {
+        }
+        arg_2 -> {
+        }
+        var_95 -> {
+        }
+        var_95 -> {
+        }
+    }
+
+    var var_97: Int
+    val var_98: Int = (+1271409465).toInt()
+    var_97 = +965120851
+    when (1.7556703E308) {
+        arg_2 -> {
+        }
+        1.1974703E308 -> {
+        }
+        arg_2 -> {
+        }
+    }
+
+    var var_99: Short = ((28084).toShort()).toShort()
+    var var_100: Float
+    var var_101: Float = (2.5945924E38F).toFloat()
+    return 2.3479274E306
+}
+
+fun main(args: Array<String>) {
+try {
+    fun_4(!false)
+    fun_4((-29151).toShort() >= ((-20493).toShort()).toShort())
+    fun_4(! (false || true) != + (2066944258 ushr -950057524) < + -1049963030 shr 1260852562 != true && false)
+    fun_4(false)
+    fun_4(6.7442965E307 > 9.4685367E307)
+    run {
+        fun_4(false)
+        var var_102 = 0
+        fun_4(false)
+        while (var_102 < 7 && !fun_6()) {
+            fun_4(']' <= 'G')
+            var_102++
+            fun_4(!fun_6())
+            fun_4(false == ((-68).toByte()).toByte() <= (-16).toByte())
+        }
+
+        when (fun_4(var_102 < var_102)) {
+            fun_4('V' >= 'Y') -> {
+            }
+            else -> {
+                var_102 %= 970287802 and (((var_102 or + -var_102) ushr 780855376)).toInt() * (var_102 and (-fun_2(7076401014111925248L, 'g', (-20433).toShort(), 1.3642312E308) xor 1954385535)) % 1362226211
+            }
+        }
+
+        fun_4(true)
+        var var_103: Int
+        fun_4(false)
+    }
+
+    when (fun_4(false)) {
+        fun_4(false) -> {
+            var var_104: Any
+        }
+        fun_4(+ (2024790482 * (1538107363 * - -464956167)) < 150325676 + 446711940 shl -771051302) -> {
+        }
+        fun_4(fun_6()) -> {
+        }
+        fun_4('a' <= 'q') -> {
+        }
+        fun_4(true < fun_6() != true) -> {
+        }
+        fun_4(true) -> {
+            fun_4(!false == !true)
+        }
+        fun_4(true) -> {
+        }
+    }
+
+    fun_4('P' > '[' == !false)
+    var var_105 = 0
+    ++var_105
+    while (var_105 < 18 && true && false) {
+        val var_106: Char = ('h').toChar()
+        var_105++
+        fun_4(false)
+        fun_4(false != !false)
+    }
+
+    val var_107: Boolean = false
+    fun_4(var_107)
+    fun_4(! ('s' >= 'i' && (var_107 && true && var_107 || var_107)))
+    --var_105
+
+    var result: String = "["
+    result += "var_105 = "
+    result += utils.Printer.print(var_105)
+    result += "\n"
+    result += "var_107 = "
+    result += utils.Printer.print(var_107)
+    result += "]\n"
+    print(result)
+} catch (ex: Exception) {
+    ex.printStackTrace()
+throw ex
+}
+}
